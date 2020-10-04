ef0e23de81d791dc2f0db60b2d049229b8a5c7ad
Munsif <munsifalimisri69@gmail.com>
2020-10-04 01:42:40 -0700

    middleWare

---
+++ E_staticHtmlUsingMiddleWare#4/E_staticHtmlMid.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//using express middle ware
+var absolutePath = path.join(__dirname, "/assets");
+app.use(express.static(absolutePath));
+
+app.get('/', (req, res) => {
+    res.send("this is home page");
+});
+app.get('/contect', (req, res) => {
+    res.send("this is contect page");
+});
+app.get('/about', (req, res) => {
+    res.send("this is about page");
+});
+//liste to the port where req is comming
+app.listen('8000', () => {
+    console.log('listening onport 8000');
+});
