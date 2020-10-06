b078ebcaebd43f1633c69d00076804ff6ffcc10e
Munsif <munsifalimisri69@gmail.com>
2020-10-06 12:37:57 -0700

    includes some buttonClick script

--- E_templeteEngineAddDynamicCon#6/assets/js/index.js
+++ E_templeteEngineAddDynamicCon#6/assets/js/index.js
@@ -3,5 +3,5 @@ document.getElementById("myButton").onclick = function () {
     location.href = "http://localhost:8000/about";
 }
 document.getElementById("myButtonHome").onclick = function () {
-    location.href = "http://www.youtube.com";
+    location.href = "http://localhost:8000/";
 }
