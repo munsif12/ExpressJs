544860098577bac7415e7a2cac08e8117bc3da31
Munsif <munsifalimisri69@gmail.com>
2020-10-06 12:19:05 -0700

    includes some button click  script

---
+++ E_templeteEngineAddDynamicCon#6/assets/js/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById("myButton").onclick = function () {
+    location.href = "http://localhost:8000/about";
+}
+document.getElementById("myButtonHome").onclick = function () {
+    location.href = "http://www.youtube.com";
+}
